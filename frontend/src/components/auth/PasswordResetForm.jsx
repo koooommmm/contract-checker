3c6fd441c20d92eabdbaa93521a60387a64b5979
koooommmm <nico.pika17@gmail.com>
2024-04-27 21:39:07 +0900

    update: パスワードリセット画面のレイアウトを修正

--- frontend/src/components/auth/PasswordResetForm.jsx
+++ frontend/src/components/auth/PasswordResetForm.jsx
@@ -4,30 +4,57 @@ import { auth } from '../../firebase/firebaseConfig';
 
 const PasswordResetForm = () => {
   const [email, setEmail] = useState('');
+  const [isSended, setIsSended] = useState(false);
   const [sendPasswordResetEmail, sending, _, error] =
     useSendPasswordResetEmail(auth);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     await sendPasswordResetEmail(email);
-    alert('パスワード再設定用のリンクをメールで送信いたしました。');
+    setIsSended(true);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder='メールアドレス'
-          required
-        />
-        <button type='submit' disabled={sending}>
-          送信
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
+    <div className='min-h-screen bg-gray-100 flex justify-center'>
+      <div className='mt-36 bg-white p-8 rounded shadow-md w-5/12 h-2/5'>
+        <h1 className='text-xl font-bold mb-4 text-center'>パスワード再設定</h1>
+        {error && (
+          <p className='p-3 my-10 text-center text-red-500 rounded bg-red-100'>
+            パスワード再設定用リンクの送信に失敗しました。
+          </p>
+        )}
+        {isSended && (
+          <p className='p-3 my-10 text-center text-green-700 rounded bg-green-100'>
+            パスワード再設定用リンクを送信しました。
+          </p>
+        )}
+        <form onSubmit={handleSubmit} className='space-y-6'>
+          <div>
+            <label className='text-sm font-semibold' htmlFor='email'>
+              メールアドレス
+            </label>
+            <input
+              className='mt-1 w-full p-2 border border-gray-300 rounded'
+              id='email'
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='メールアドレス'
+              required
+            />
+          </div>
+
+          <div className='flex justify-center'>
+            <button
+              type='submit'
+              disabled={sending}
+              className='w-1/3 bg-red-500 text-white p-3 rounded-full hover:bg-red-600 disabled:bg-red-300'
+            >
+              送信
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
