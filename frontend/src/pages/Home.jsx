b685faf91657461a128633e348cee75233aff2b4
koooommmm <nico.pika17@gmail.com>
2024-05-13 22:12:22 +0900

    fix: 一覧表示のレイアウトを修正

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import ContractsList from '../components/common/ContractsList';
-import FileUpload from '../components/FileUpload';
 
 const Home = () => {
   return (
-    <div>
-      <ContractsList />
-      <FileUpload />
+    <div className='min-h-screen bg-gray-100'>
+      <div className='flex items-center justify-between p-5 max-w-4xl mx-auto'>
+        <div className='text-lg font-bold'>請求書リスト</div>
+        <button className='bg-red-500 text-white p-3 rounded-full hover:bg-red-600 disabled:bg-red-300'>
+          新規追加
+        </button>
+      </div>
+      <div className=' max-w-4xl mx-auto'>
+        <ContractsList />
+      </div>
     </div>
   );
 };
